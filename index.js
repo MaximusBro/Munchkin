cbb77c8be5171c5d469842d3117253717c69176c
Voronov-Ivan <78203791+Voronov-Ivan@users.noreply.github.com>
2021-01-29 22:28:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+// Підключення модулів необхідних для запуску сайта і бекенду
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io');
+//Кінець
+server.listen(3000);                      //Прописано який порт буде відслідковувати сервер
+
+app.get('/',function (request,respons) {  //request = Запрос; respons =  ответ
+    respons.sendFile(__dirname + '/index.html');          // Дозволяэ відправити деякий файл (__dirname = деректорыя в якій ми знаходимся)
+});// Який URL-фдрес ми відслідковіємо
