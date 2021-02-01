52ae0f3b1e93831c81f63caed05d22ddbab1df89
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-02-01 22:31:09 +0200

    Змыни з можливыстю входити в комнату

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
 
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+// app.get("/start_game", urlencodedParser, function (request, response) {
+//     response.sendFile(__dirname + "/start_game.html");
+// });
+a = 0;
+app.post("/start_game", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.sendFile(__dirname + "/start_game.html")
+    console.log("You connect");
+});
 //
 const server = require('http').Server(app); //
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
