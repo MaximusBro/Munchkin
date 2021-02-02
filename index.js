39541b4a916493aecf9ab9c20aba36d2cc80c5fb
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-02-02 17:10:31 +0200

    New Filles

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname+'/images'));
+
 const path = require('path');
-const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-// app.get("/start_game", urlencodedParser, function (request, response) {
-//     response.sendFile(__dirname + "/start_game.html");
-// });
-a = 0;
-app.post("/start_game", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.sendFile(__dirname + "/start_game.html")
-    console.log("You connect");
-});
-//
-const server = require('http').Server(app); //
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
-//
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+var server = app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
-app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/about.html'));
+app.use(express.static("./views"));
+
+var io = require('socket.io')(server);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
-app.get('/game', function(req, res) {
-    res.sendFile(path.join(__dirname + '/game.html'));
+
+app.get('/', function (req, res) {
+    var path = __dirname + '/game.html';
+    console.log(path);
+    res.sendFile(path);
+});
+
+app.post("/start_game", urlencodedParser, function (req, res) {
+    if(!req.body) return res.sendStatus(400);
+    res.sendFile(__dirname + '/views/start_game.html');
+});
+var players = []
+io.on('connection', function(socket) {
+    players.push(socket);
+    console.log("Connected");
+    io.emit('players_in_room',players.length);
+
+    socket.on('disconnect', function () {
+    players.splice(players.indexOf(socket),1);
+    console.log("disconnected");
+    io.emit('players_in_room',players.length);
+    });
 });
