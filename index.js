f9c4a926b23293b34a7ed66760ba3b3195bf4692
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-02-04 11:17:08 +0200

    Зроблено повночынну комнату,з паролем

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+//
+app.use(express.urlencoded());
+app.use(express.json());
+//
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/images'));
 
@@ -24,19 +28,46 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/game.html');
 });
 
-app.post("/start_game", urlencodedParser, function (req, res) {
+app.post("/munchkin", urlencodedParser, function (req, res) {
+  if (players.length == 0) {
     if(!req.body) return res.sendStatus(400);
-    res.sendFile(__dirname + '/start_game.html');
+    res.sendFile(__dirname + '/munchkin.html');
+  }
+    else {
+      console.log("Комната занята");
+    }
+});
+app.post("/munchkin_start", urlencodedParser, function (req, res) {
+  if (req.body.pris == password) {
+    if(!req.body) return res.sendStatus(400);
+    res.sendFile(__dirname + '/munchkin.html');
+  }
+    else {
+      console.log("Комната не создана");
+    }
+});
+app.post("/munchkin_set_password", urlencodedParser, function (req, res) {
+    password = req.body.pass;
+    res.sendFile(__dirname + '/munchkin.html');
 });
 var players = []
+var password = ""
 io.on('connection', function(socket) {
     players.push(socket);
     console.log("Connected");
+    //
     io.emit('players_in_room',players.length);
-
+    // io.emit('players_in_room',players.length)
     socket.on('disconnect', function () {
     players.splice(players.indexOf(socket),1);
     console.log("disconnected");
     io.emit('players_in_room',players.length);
+
+    // socket.on('open_room',function (pass) {
+    //   password = pass;
+    // })
+    // socket.on('send_name',function (name) {
+    //   io.emit('set_name',name);
+    // })
     });
 });
